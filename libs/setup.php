0b725104bd7aeabf8d729bed13d9cd50b30bf28c
CarmenMariaNicolas <carmen_lmk@hotmail.com>
2023-02-12 19:20:13 +0100

    versus 004

--- libs/setup.php
+++ libs/setup.php
@@ -1,6 +1,6 @@
 <?php
 // Obtiene la instancia del objeto que guarda los datos de configuración
-//$config = Config::singleton();
+$config = Config::singleton();
 
 // Carpetas para los Controladores, los Modelos y las Vistas
 $config->set('controllersFolder', 'controllers/');
@@ -9,7 +9,7 @@ $config->set('viewsFolder', 'views/');
 
 // Parámetros de conexión a la BD
 $config->set('dbhost', 'containers-us-west-150.railway.app:5856/railway');
-$config->set('dbname', 'framework');
+$config->set('dbname', 'railway');
 $config->set('dbuser', 'super');
 $config->set('dbpass', '123456');
 ?>
